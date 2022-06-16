60e6beb7f0975a0c055ee86e1d3ff2ca4a952cc6
luis marin <luismarin@luiss-MBP.lan>
2022-06-15 23:07:01 -0400

    Weather App2

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function TopButtons() {
+    
+    const cities =[
+        {
+            id: 1,
+            title: "Los Angeles",
+        },
+        {
+            id: 2,
+            title: "Atlanta",
+        },
+        {
+            id: 3,
+            title: "New York",
+        },
+        {
+            id: 4,
+            title: "Miami",
+        },
+        {
+            id: 5,
+            title: "Chicago",
+        },
+    ]
+  return (
+    <div className='flex items-center justify-center my-6'>TopButtons</div>
+  )
+}
+
+export default TopButtons
